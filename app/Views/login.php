7c481028a9bdec7b3e467acc3ae51bc2bf9fa426
Fran <francobarrientos@gmail.com>
2024-06-01 18:08:03 -0300

    v2

--- app/Views/login.php
+++ app/Views/login.php
@@ -2,6 +2,7 @@
 background-color: #002448">
     <div class="login-container" style="background-color: rgba(255, 255, 255, 0.8); padding: 30px; border-radius: 10px; text-align: center;max-width: 400px; margin: auto; border: 1px solid #ccc; border-radius: 10px;">
         <h2>Iniciar sesión</h2>
+        
         <form action="api/login" method="post">
             <input type="text" name="usuario" placeholder="Usuario" style="width: 100%; padding: 10px; margin: 10px 0; box-sizing: border-box; border: 1px solid #ccc; border-radius: 5px;" required><br>
             <input type="password" name="pass" placeholder="Contraseña" style="width: 100%; padding: 10px; margin: 10px 0; box-sizing: border-box; border: 1px solid #ccc; border-radius: 5px;" required><br>
